dfc4d28531d94ed335220ecdb8c373aa09e73523
liusimin <liusimin@tungee.com>
2018-10-07 16:27:44 +0800

    公告弹出层布局

--- src/components/Header/components/Bulletin.vue
+++ src/components/Header/components/Bulletin.vue
@@ -6,21 +6,59 @@
 					<h1 class="detail-title">{{seller.name}}</h1>
 					<star :score="seller.score" :size="48" :total="5"></star>
 				</div>
+				<div class="floor">
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">优惠信息</div>
+						<div class="line"></div>
+					</div>
+					<div class="content-box">
+						<ul class="supports" v-if="seller.supports">
+							<support-item
+								v-for="(item, index) of seller.supports"
+								:key="index"
+								:item="item">
+							</support-item>
+						</ul>
+					</div>
+				</div>
+				<div class="floor">
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">商家公告</div>
+						<div class="line"></div>
+					</div>
+					<div class="content-box">
+						<div class="bulletin-text">
+							{{seller.bulletin}}
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="close"></div>
+		<div class="close">
+			<i class="icon-close"></i>
+		</div>
 	</div>
 </template>
 
 <script>
 import Star from '@/components/Star/Star';
+import SupportItem from './SupportItem';
+
 export default {
 	name: 'Bulletin',
 	components: {
 		Star,
+		SupportItem,
 	},
 	props: {
 		seller: Object,
+	},
+	computed: {
+		iconImg() {
+			return ['decrease', 'guaratee', 'guaratee', 'invoice', 'special'][this.seller.supports.type];
+		},
 	},
 };
 </script>
@@ -33,23 +71,46 @@ export default {
 		left 0
 		right 0
 		z-index 9
+		line-height 1
 		background-color rgba(7,17,27,.8)
 		.detail-wrapper
 			min-height 100%
 			.detail-main
-				margin-top 1.28rem
-				padding-bottom 1.28rem
+				padding 1.28rem 0
 				text-align center
 				.detail-header
-					padding-bottom .56rem
 					.detail-title
 						margin-bottom .32rem
 						font-size .32rem
 						font-weight 700
 						line-height .32rem
+				.floor
+						width 80%
+						margin .56rem auto 0
+					.title
+						display flex
+					.line
+						position relative
+						flex 1
+						top -.14rem
+						border-bottom 1px solid rgba(255,255,255,0.2)
+					.text
+						padding 0 .24rem
+						font-size .28rem
+						font-weight 700
+				.content-box
+					padding .48rem .24rem 0
+					text-align left
+					.bulletin-text
+						font-size .24rem
+						font-weight 200
+						line-height .48rem
 		.close
 			margin-top -1.28rem
 			width 100%
 			height 1rem
+			text-align center
+			font-size .64rem
+			color rgba(255,255,255,.5)
 </style>
 
---
+++ src/components/Header/components/SupportItem.vue
@@ -0,0 +1,49 @@
+<template>
+	<li class="support-item">
+		<span :class="['icon', iconImg]"></span>
+		<span class="description">{{item.description}}</span>
+	</li>
+</template>
+
+<script>
+export default {
+	name: 'SupportItem',
+	props: {
+		item: Object,
+	},
+	computed: {
+		iconImg() {
+			return ['decrease', 'discount', 'guarantee', 'invoice', 'special'][this.item.type];
+		},
+	},
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/style/mixin.styl';
+
+.support-item
+	margin-bottom .24rem
+	.icon
+		display inline-block
+		width .48rem
+		height .48rem
+		margin-right .12rem
+		background-size .48rem .48rem
+		vertical-align middle
+		background-repeat no-repeat
+		&.decrease
+			bg-img('../assets/decrease_2')
+		&.discount
+			bg-img('../assets/discount_2')
+		&.guarantee
+			bg-img('../assets/guarantee_2')
+		&.invoice
+			bg-img('../assets/invoice_2')
+		&.special
+			bg-img('../assets/special_2')
+	.description
+		font-size .24rem
+		font-weight 200
+</style>
+
